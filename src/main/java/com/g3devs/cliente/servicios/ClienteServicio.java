3cc9a069198074ed60b080db6c1592dcef8e79e0
mario <mario@LAPTOP-V5PN36QL>
2021-02-09 17:16:18 +0100

    MarioDAM

--- src/main/java/com/g3devs/cliente/servicios/ClienteServicio.java
+++ src/main/java/com/g3devs/cliente/servicios/ClienteServicio.java
@@ -1,6 +1,7 @@
 package com.g3devs.cliente.servicios;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -34,6 +35,12 @@ public class ClienteServicio extends Thread{
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		try(Socket clienteSocket = new Socket()){
+			InetSocketAddress addr = new InetSocketAddress("", 0000);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void leerRespuesta(Socket clientSocket) {
@@ -46,6 +53,7 @@ public class ClienteServicio extends Thread{
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 	private void enviarMensaje(Socket clientSocket) {
